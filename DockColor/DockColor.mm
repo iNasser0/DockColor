55863683245c05756c237495aed8e0f7e4e5c81d
iNasser0 <nasser1234517@icloud.com>
2014-12-18 20:36:51 +0300

    First init

---
+++ DockColor/DockColor.mm
@@ -0,0 +1,72 @@
+#line 1 "/Users/iNasser/Desktop/DockColorTweak/DockColor/DockColor.xm"
+#import <UIKit/UIKit.h>
+#define PREFSFILENAME "com.inasser.dockcolor"
+
+#define kDefaultWhiteColor [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1.0f]
+
+
+BOOL enabled;
+UIColor *color;
+float BGalpha;
+
+
+#include <logos/logos.h>
+#include <substrate.h>
+@class SBDockView; 
+static void (*_logos_orig$_ungrouped$SBDockView$layoutSubviews)(SBDockView*, SEL); static void _logos_method$_ungrouped$SBDockView$layoutSubviews(SBDockView*, SEL); 
+
+#line 12 "/Users/iNasser/Desktop/DockColorTweak/DockColor/DockColor.xm"
+
+
+static void _logos_method$_ungrouped$SBDockView$layoutSubviews(SBDockView* self, SEL _cmd) {
+    _logos_orig$_ungrouped$SBDockView$layoutSubviews(self, _cmd);
+    if (enabled == YES) {
+    
+        UIView *bGView = MSHookIvar<UIView *>(self, "_backgroundView");
+        bGView.alpha = BGalpha / 20.0;
+        
+        UIView *viewToAdd = [[UIView alloc] initWithFrame:bGView.frame];
+        viewToAdd.backgroundColor = color;
+        
+    [bGView addSubview:viewToAdd];
+        }
+   
+
+    
+    }
+
+
+static UIColor* parseColorFromPreferences(NSString* string) {
+    NSArray *prefsarray = [string componentsSeparatedByString: @":"];
+    NSString *hexString = [prefsarray objectAtIndex:0];
+    double alpha = [[prefsarray objectAtIndex:1] doubleValue];
+    
+    unsigned rgbValue = 0;
+    NSScanner *scanner = [NSScanner scannerWithString:hexString];
+    [scanner setScanLocation:1];
+    [scanner scanHexInt:&rgbValue];
+    return [[UIColor alloc] initWithRed:((rgbValue & 0xFF0000) >> 16)/255.0 green:((rgbValue & 0xFF00) >> 8)/255.0 blue:(rgbValue & 0xFF)/255.0 alpha:alpha];
+}
+static void reloadPrefs() {
+  
+   CFPreferencesAppSynchronize(CFSTR(PREFSFILENAME));
+   enabled =  !CFPreferencesCopyAppValue(CFSTR("enabled"), CFSTR(PREFSFILENAME)) ? YES : [(id)CFBridgingRelease(CFPreferencesCopyAppValue(CFSTR("enabled"), CFSTR(PREFSFILENAME))) boolValue];
+    color =!CFPreferencesCopyAppValue(CFSTR("kColor"), CFSTR(PREFSFILENAME)) ? kDefaultWhiteColor : parseColorFromPreferences((id)CFPreferencesCopyAppValue(CFSTR("kColor"), CFSTR(PREFSFILENAME)));
+BGalpha = !CFPreferencesCopyAppValue(CFSTR("alpha"), CFSTR(PREFSFILENAME)) ? 1 : [(id)CFBridgingRelease(CFPreferencesCopyAppValue(CFSTR("alpha"), CFSTR(PREFSFILENAME))) floatValue];
+ 
+}
+
+
+static __attribute__((constructor)) void _logosLocalCtor_544b3284() {
+    reloadPrefs();
+    CFNotificationCenterAddObserver(CFNotificationCenterGetDarwinNotifyCenter(),
+                                    NULL,
+                                    (CFNotificationCallback)reloadPrefs,
+                                    CFSTR("com.inasser.dockcolor/prefschanged"),
+                                    NULL,
+                                    CFNotificationSuspensionBehaviorDeliverImmediately);
+   
+}
+static __attribute__((constructor)) void _logosLocalInit() {
+{Class _logos_class$_ungrouped$SBDockView = objc_getClass("SBDockView"); MSHookMessageEx(_logos_class$_ungrouped$SBDockView, @selector(layoutSubviews), (IMP)&_logos_method$_ungrouped$SBDockView$layoutSubviews, (IMP*)&_logos_orig$_ungrouped$SBDockView$layoutSubviews);} }
+#line 63 "/Users/iNasser/Desktop/DockColorTweak/DockColor/DockColor.xm"
---
+++ DockColor/DockColor.xm
@@ -0,0 +1,62 @@
+#import <UIKit/UIKit.h>
+#define PREFSFILENAME "com.inasser.dockcolor"
+
+#define kDefaultWhiteColor [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1.0f]
+
+
+BOOL enabled;
+UIColor *color;
+float BGalpha;
+
+
+%hook SBDockView
+
+-(void)layoutSubviews {
+    %orig;
+    if (enabled == YES) {
+    
+        UIView *bGView = MSHookIvar<UIView *>(self, "_backgroundView");
+        bGView.alpha = BGalpha / 20.0;
+        
+        UIView *viewToAdd = [[UIView alloc] initWithFrame:bGView.frame];
+        viewToAdd.backgroundColor = color;
+        
+        [bGView addSubview:viewToAdd];
+}
+   
+
+    
+}
+
+%end
+static UIColor* parseColorFromPreferences(NSString* string) {
+    NSArray *prefsarray = [string componentsSeparatedByString: @":"];
+    NSString *hexString = [prefsarray objectAtIndex:0];
+    double alpha = [[prefsarray objectAtIndex:1] doubleValue];
+    
+    unsigned rgbValue = 0;
+    NSScanner *scanner = [NSScanner scannerWithString:hexString];
+    [scanner setScanLocation:1];
+    [scanner scanHexInt:&rgbValue];
+    return [[UIColor alloc] initWithRed:((rgbValue & 0xFF0000) >> 16)/255.0 green:((rgbValue & 0xFF00) >> 8)/255.0 blue:(rgbValue & 0xFF)/255.0 alpha:alpha];
+}
+static void reloadPrefs() {
+  
+   CFPreferencesAppSynchronize(CFSTR(PREFSFILENAME));
+   enabled =  !CFPreferencesCopyAppValue(CFSTR("enabled"), CFSTR(PREFSFILENAME)) ? YES : [(id)CFBridgingRelease(CFPreferencesCopyAppValue(CFSTR("enabled"), CFSTR(PREFSFILENAME))) boolValue];
+    color =!CFPreferencesCopyAppValue(CFSTR("kColor"), CFSTR(PREFSFILENAME)) ? kDefaultWhiteColor : parseColorFromPreferences((id)CFPreferencesCopyAppValue(CFSTR("kColor"), CFSTR(PREFSFILENAME)));
+BGalpha = !CFPreferencesCopyAppValue(CFSTR("alpha"), CFSTR(PREFSFILENAME)) ? 1 : [(id)CFBridgingRelease(CFPreferencesCopyAppValue(CFSTR("alpha"), CFSTR(PREFSFILENAME))) floatValue];
+ 
+}
+
+
+%ctor {
+    reloadPrefs();
+    CFNotificationCenterAddObserver(CFNotificationCenterGetDarwinNotifyCenter(),
+                                    NULL,
+                                    (CFNotificationCallback)reloadPrefs,
+                                    CFSTR("com.inasser.dockcolor/prefschanged"),
+                                    NULL,
+                                    CFNotificationSuspensionBehaviorDeliverImmediately);
+   
+}
